16d636c11d17ef64a185fd30e0fc204470114d2d
Jens-Erik Weber <Jens-Erik.Weber@passiv.de>
2020-08-05 17:00:10 +0200

    Kick out Python compatibility, esp. for Django 3 compatibility
    from django.utils.encoding import python_2_unicode_compatible

--- aldryn_categories/__init__.py
+++ aldryn_categories/__init__.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-
 __version__ = '1.2.0'
-
 
 default_app_config = 'aldryn_categories.apps.AldrynCategories'
--- aldryn_categories/models.py
+++ aldryn_categories/models.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import unicode_literals
-
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.html import escape
 from django.utils.translation import ugettext_lazy as _
 
