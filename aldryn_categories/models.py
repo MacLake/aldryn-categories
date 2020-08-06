ffa9ea22ae3377d7f7b60c89c17403b71cd91f02
Jens-Erik Weber <Jens-Erik.Weber@passiv.de>
2020-08-06 13:14:29 +0200

    Use six instead of django.utils.encoding for python_2_unicode_compatible

--- aldryn_categories/models.py
+++ aldryn_categories/models.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.html import escape
 from django.utils.translation import ugettext_lazy as _
 
